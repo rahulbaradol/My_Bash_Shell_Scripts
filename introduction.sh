86f2fdb5a3703e2aabe1715e80f5c26675ca5bed
rahulbaradol <rahulbaradol142@gmail.com>
2018-07-23 11:01:52 +0000

    Initial Commit

---
+++ introduction.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+echo "Hi, welcome"
+read -p "Enter your first name: " firstname
+read -p "Enter your last name: " lastname
+read -p "Enter your age: " age
+
+echo "Your full name is" $firstname $lastname "and your age is" $age
---
+++ make_Multiple_Directories.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+mkdir $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+echo "Directorie(s) created successfully"
